6910e4d85b44bf765f5f62ce8c5bdfb1a872f5d7
GtoXic <root@toxic-productions.com>
2012-02-17 19:55:11 -0700

    Automated Commit

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@
 			echo($info['module']." loaded");
 			$params = array();
 			foreach($info['loaded_params'] as $param => $value){
-				$params[$param] = str_replace(array("%nick","%chan","%ex","%host","%server","%botnick"),array($nick,$chan,$ex,$host,$server,globals::config['nick']),$value);
+				$params[$param] = str_replace(array("%nick","%chan","%ex","%host","%server","%botnick"),array($nick,$chan,$ex,$host,globals::config['server'],globals::config['nick']),$value);
 			}
 			$info['loaded']($info['loaded_params']);
 		}
