8e7b595314f84f225d7175da1c2f0b877b386793
GtoXic <root@toxic-productions.com>
2012-02-17 20:10:55 -0700

    Removed files

--- run.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if($argc < 3){
-		die("Usage: {$argv[0]} <host[:port]> <nick> <channel>\r\nSSL is currently not supported. It may be added in a later version.\r\n");
-	}else{
-		include("./bot.php");
-		$bot = new bot($argv[0],$argv[1]);
-	}
-?>
